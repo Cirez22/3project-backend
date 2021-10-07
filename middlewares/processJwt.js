aefda13fd6b8597b9e237a7b5fa7672fcbc4825c
Matias Pancorvo <matiaspancorvo@MacBook-Air-de-Matias.local>
2021-10-07 17:20:38 -0400

    sign up

--- middlewares/processJwt.js
+++ middlewares/processJwt.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
 
+const User = require('../models/UserModel');
+
 const generateJwt = (id) => {
   return new Promise((resolve, reject) => {
     const data = { uid: id };
@@ -18,6 +20,40 @@ const generateJwt = (id) => {
   });
 };
 
+
+const validateJwt = async (req, res, next) => {
+  const token = req.header('Authorize');
+  if (!token) return res.status(400).json({ message: 'Token Not Found' });
+  try {
+    const { uid } = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = await User.findById(uid)
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid Token" })
+  }
+}
+
+const revalidateJwt = async (req, res) => {
+  const user = req.user;
+  const token = await generateJwt(user._id)
+  try {
+    return res.json({ user, token })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const isAdmin = async (req,res,next) => {
+  if (!req.user) {
+    return res.status(500).json({message:`User ${name} does not have privilages`})
+  }
+  next();
+}
+
+
 module.exports = {
+  validateJwt,
+  revalidateJwt,
+  isAdmin,
   generateJwt
 }
--- models/UserModel.js
+++ models/UserModel.js
@@ -2,12 +2,6 @@ const {Schema, model} = require('mongoose')
 
 const UserSchema = Schema(
     {
-
-        name: {
-            type: String,
-            required: true,
-            trim: true
-        },
         email: {
             type: String,
             required: true,
